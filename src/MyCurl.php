6b0174f1fe81576ef09b2c1a2e784c38762309e5
mjy191 <mjy191@163.com>
2022-09-13 15:46:41 +0800

    格式化代码

--- src/MyCurl.php
+++ src/MyCurl.php
@@ -1,11 +1,13 @@
 <?php
+
 namespace Mjy191\MyCurl;
 
 use Mjy191\Enum\Enum;
 use App\Exceptions\ApiException;
 use Mjy191\MyLogs\MyLogs;
 
-class MyCurl {
+class MyCurl
+{
 
     /**
      * curl请求
@@ -19,33 +21,34 @@ class MyCurl {
      * @return bool|string
      * @throws ApiException
      */
-    public static function send($url, $method='get', $params='', array $header=null, $resLog=true, $reqLog=true, $errLog=true){
-        $reqLog && (MyLogs::write('curl',"url[{$url}] method[{$method}] params[".MyLogs::toString($params)."] header[".MyLogs::toString($header)."]"));
+    public static function send($url, $method = 'get', $params = '', array $header = null, $resLog = true, $reqLog = true, $errLog = true)
+    {
+        $reqLog && (MyLogs::write('curl', "url[{$url}] method[{$method}] params[" . MyLogs::toString($params) . "] header[" . MyLogs::toString($header) . "]"));
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)');
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);     // 设置超时限制防止死循环
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     // 获取的信息以文件流的形式返回
-        if($header){
+        if ($header) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header); //模拟的header头
         }
-        if($method=='post'){
+        if ($method == 'post') {
             curl_setopt($ch, CURLOPT_POST, true); // 发送一个常规的Post请求
-            if($params){
+            if ($params) {
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $params);     // Post提交的数据包
             }
         }
         $result = curl_exec($ch);
-        $resLog && (MyLogs::write('curl response',$result));
+        $resLog && (MyLogs::write('curl response', $result));
         $getInfo = curl_getinfo($ch);
         $errno = curl_errno($ch);
         $error = curl_error($ch);
-        if($errno!=0){
-            $errLog && (MyLogs::write('curl error',"errno[".MyLogs::toString($errno)."] error[".MyLogs::toString($error)."] info[".MyLogs::toString($getInfo)."]"));
-            throw new ApiException('系统错误',Enum::erCodeServer);
+        if ($errno != 0) {
+            $errLog && (MyLogs::write('curl error', "errno[" . MyLogs::toString($errno) . "] error[" . MyLogs::toString($error) . "] info[" . MyLogs::toString($getInfo) . "]"));
+            throw new ApiException('系统错误', Enum::erCodeServer);
         }
         curl_close($ch);
         return $result;
